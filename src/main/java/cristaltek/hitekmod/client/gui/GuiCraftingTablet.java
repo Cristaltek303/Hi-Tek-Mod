350c8aa660b23481278c7845f1b710c7a352f91a
FantasyTeddy <matthiasbaer@bluewin.ch>
2015-08-27 19:53:38 +0200

    Re-add transparency to crafting tablet GUI

--- src/main/java/cristaltek/hitekmod/client/gui/GuiCraftingTablet.java
+++ src/main/java/cristaltek/hitekmod/client/gui/GuiCraftingTablet.java
@@ -2,6 +2,7 @@ package cristaltek.hitekmod.client.gui;
 
 import org.lwjgl.opengl.GL11;
 
+import cristaltek.hitekmod.HiTekMod;
 import cristaltek.hitekmod.client.inventory.ContainerCraftingTablet;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -15,26 +16,15 @@ public class GuiCraftingTablet extends GuiContainer {
 		xSize = 220;
 		ySize = 193;
 	}
-	
-//	@Override
-//	public void drawScreen(int x, int y, float ticks) {
-//		int guiX = (width - guiWidth) / 2;
-//		int guiY = (height - guiHeight) / 2;
-//		GL11.glColor4f(1, 1, 1, 0.85F); //Alpha = Transparent
-//		//drawDefaultBackground();
-//		mc.renderEngine.bindTexture(new ResourceLocation(cristaltek.hitekmod.HiTekMod.MOD_ID, "textures/gui/guiCraftingTablet.png"));
-//		drawTexturedModalRect(guiX, guiY, 0, 0, guiWidth, guiHeight);
-//		
-//		super.drawScreen(x, y, ticks);
-//	}
 
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float ticks, int x, int y) {
 		int guiX = (width - xSize) / 2;
 		int guiY = (height - ySize) / 2;
-		GL11.glColor4f(1, 1, 1, 0.85F); //Alpha = Transparent
-		//drawDefaultBackground();
-		mc.renderEngine.bindTexture(new ResourceLocation(cristaltek.hitekmod.HiTekMod.MOD_ID, "textures/gui/guiCraftingTablet.png"));
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.9F); //Alpha = Transparent
+		mc.getTextureManager().bindTexture(new ResourceLocation(HiTekMod.MOD_ID, "textures/gui/guiCraftingTablet.png"));
 		drawTexturedModalRect(guiX, guiY, 0, 0, xSize, ySize);
 	}
 }

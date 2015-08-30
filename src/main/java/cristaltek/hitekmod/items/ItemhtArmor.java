556299895f1b606c3a6473cf45dca2642f47b115
Cristaltek303 <alain.gusset@gmail.com>
2015-08-30 13:05:50 +0200

    first try to add flight capabilities to armor
    (without energy usage, code only half correct => flight works even if
    armor not equipped...)

--- src/main/java/cristaltek/hitekmod/items/ItemhtArmor.java
+++ src/main/java/cristaltek/hitekmod/items/ItemhtArmor.java
@@ -1,10 +1,14 @@
 package cristaltek.hitekmod.items;
 
+import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cristaltek.hitekmod.HiTekMod;
+import cristaltek.hitekmod.reference.Reference;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class ItemhtArmor extends ItemArmor {
@@ -31,4 +35,41 @@ public class ItemhtArmor extends ItemArmor {
 		}
 		return "ht:textures/models/armor/XLF-1 Armor_layer_1.png";
 	}
+	
+	//Fly-Test----------------------------------------------------------------------------------------------------
+	
+	
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemstack)
+		{
+				if (player.getCurrentArmor(3)!=null && 
+					player.getCurrentArmor(2)!=null && 
+					player.getCurrentArmor(1)!=null && 
+					player.getCurrentArmor(0)!=null)
+				{
+					ItemStack helmet = player.getCurrentArmor(3);
+					ItemStack chestplate = player.getCurrentArmor(2);
+					ItemStack leggings = player.getCurrentArmor(1);
+					ItemStack boots = player.getCurrentArmor(0);
+					
+					if(helmet.getItem()==ModItems.htHelmet &&
+						chestplate.getItem()==ModItems.htChestplate &&
+						leggings.getItem()==ModItems.htLeggings &&
+						boots.getItem()==ModItems.htBoots)
+					{
+						player.capabilities.allowFlying = true;
+						player.capabilities.setFlySpeed(2.0F);
+						player.fallDistance = 0.0F;
+					}
+					else
+					{
+						
+
+						
+					}
+				}
+		}
+		
 }
+

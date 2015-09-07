d029c7273a056b4f6864bbd41b9724dbed6aabb6
FantasyTeddy <matthiasbaer@bluewin.ch>
2015-09-07 10:32:32 +0200

    Add energy to OPSword

--- src/main/java/cristaltek/hitekmod/Configs.java
+++ src/main/java/cristaltek/hitekmod/Configs.java
@@ -11,6 +11,9 @@ public class Configs
 	public static int TDTool_maxEnergy;
 	public static int TDTool_energyPerUse;
 	
+	public static int OPSword_maxEnergy;
+	public static int OPSword_energyPerHit;
+	
 	public static int magnet_strength;
 	public static int htArmor_maxEnergy;
 	
@@ -26,6 +29,9 @@ public class Configs
 	{
 		TDTool_maxEnergy = config.getInt("maxEnergy", "TDTool", 1000000, 10000, 10000000, "The maximum amount of energy that the TDTool can hold");
 		TDTool_energyPerUse = config.getInt("energyPerUse", "TDTool", 100, 10, 1000, "The amount of energy the TDTool consumes per use");
+		
+		OPSword_maxEnergy = config.getInt("maxEnergy", "OPSword", 1000000, 10000, 10000000, "The maximum amount of energy that the OPSword can hold");
+		OPSword_energyPerHit = config.getInt("energyPerHit", "OPSword", 100, 10, 1000, "The amount of energy the OPSword consumes per hit");
 		
 		magnet_strength = config.getInt("magnetStrength", "misc", 10, 5, 15, "How far the magnet can reach");
 		htArmor_maxEnergy = config.getInt("maxEnergy", "htArmor", 1000000, 1000, 10000000, "The maximum amount of energy that the XLF-1 armor can hold");
--- src/main/java/cristaltek/hitekmod/items/ItemOPSword.java
+++ src/main/java/cristaltek/hitekmod/items/ItemOPSword.java
@@ -1,19 +1,29 @@
 package cristaltek.hitekmod.items;
 
+import java.util.List;
+
+import cofh.api.energy.IEnergyContainerItem;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import cristaltek.hitekmod.Configs;
 import cristaltek.hitekmod.HiTekMod;
-import net.minecraft.item.Item;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.EnumHelper;
 
-public class ItemOPSword extends ItemSword
+public class ItemOPSword extends ItemSword implements IEnergyContainerItem
 {
 	//Sword Material
-	public static final Item.ToolMaterial OPSwordmaterial = EnumHelper.addToolMaterial("OPSwordmaterial", 0, -1, 0.0F, 96, 50);
+	public static final ToolMaterial material = EnumHelper.addToolMaterial("OPSwordMaterial", 0, 0, 0.0F, 96, 50);
 	
 	public ItemOPSword(String name)
 	{
-		super(OPSwordmaterial);
+		super(material);
 		setUnlocalizedName(name);
 		setTextureName("ht:" + name);
 		setCreativeTab(HiTekMod.tabHiTekMod);
@@ -22,5 +32,91 @@ public class ItemOPSword extends ItemSword
 		//Sword characteristics
 		setMaxStackSize(1);
 		canRepair = false;
+	}
+	
+	@Override
+	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity)
+	{
+		if (getEnergyStored(stack) >= Configs.OPSword_energyPerHit)
+			return false;
+		else
+			return true;
+	}
+	
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker)
+	{
+		if (getEnergyStored(stack) >= Configs.OPSword_energyPerHit)
+			extractEnergy(stack, Configs.OPSword_energyPerHit, false);
+		
+		return true;
+	}
+	
+	// Energy implementation (with durability bar & tooltip information)
+	
+	@Override
+	public double getDurabilityForDisplay(ItemStack stack) {
+		return 1.0 - (double)this.getEnergyStored(stack) / (double)this.getMaxEnergyStored(stack);
+	}
+	
+	@Override
+	public boolean showDurabilityBar(ItemStack stack)
+	{
+		return this.getEnergyStored(stack) < this.getMaxEnergyStored(stack);
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List info, boolean debug)
+	{
+		info.add(this.getEnergyStored(stack) + " / " + this.getMaxEnergyStored(stack) + " RF");
+	}
+	
+	@Override
+	public int extractEnergy(ItemStack container, int maxExtract, boolean simulate)
+	{
+		if (container.stackTagCompound == null)
+			container.stackTagCompound = new NBTTagCompound();
+		
+		int energy = container.stackTagCompound.getInteger("Energy");
+		int energyExtracted = Math.min(energy, maxExtract);
+		
+		if (!simulate) {
+			energy -= energyExtracted;
+			container.stackTagCompound.setInteger("Energy", energy);
+		}
+		return energyExtracted;
+	}
+	
+	@Override
+	public int getEnergyStored(ItemStack container)
+	{
+		if (container.stackTagCompound == null || !container.stackTagCompound.hasKey("Energy"))
+			return 0;
+		
+		return container.stackTagCompound.getInteger("Energy");
+	}
+	
+	@Override
+	public int getMaxEnergyStored(ItemStack container)
+	{
+		return Configs.OPSword_maxEnergy;
+	}
+	
+	@Override
+	public int receiveEnergy(ItemStack container, int maxReceive, boolean simulate)
+	{
+		if (container.stackTagCompound == null)
+			container.stackTagCompound = new NBTTagCompound();
+		
+		int energy = container.stackTagCompound.getInteger("Energy");
+		int energyReceived = Math.min(Configs.OPSword_maxEnergy - energy, maxReceive);
+		
+		if (!simulate) {
+			energy += energyReceived;
+			container.stackTagCompound.setInteger("Energy", energy);
+		}
+		return energyReceived;
 	}
 }

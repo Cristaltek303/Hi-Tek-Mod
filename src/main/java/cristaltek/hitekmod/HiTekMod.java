51a6c49508ab7dc3f1dfe2f9e1ad9e3ce44d394f
Matthias Baer <matthiasbaer@bluewin.ch>
2016-09-01 17:13:01 +0200

    Initial 1.10 commit.

---
+++ src/main/java/cristaltek/hitekmod/HiTekMod.java
@@ -0,0 +1,20 @@
+package cristaltek.hitekmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = HiTekMod.MODID, version = HiTekMod.VERSION)
+public class HiTekMod
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
